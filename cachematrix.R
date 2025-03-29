1263ee1d66d2d56d744131ccadf3fcd4c45772b1
Ivanna Priscilla <ivanapriscilla16@gmail.com>
2025-03-29 11:14:20 +0700

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment 2: Lexical Scoping
 
-## Write a short comment describing this function
+
+## Make Cache Matrix Function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL  # Initialize the inverse as NULL
+  
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL  # Reset inverse cache when setting a new matrix
+  }
+  
+  get <- function() x  # Retrieve the matrix
+  
+  setinverse <- function(inverse) inv <<- inverse  # Cache the inverse
+  
+  getinverse <- function() inv  # Retrieve the cached inverse
+  
+  list(set = set, get = get, 
+       setinverse = setinverse, 
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+
+## Cache Solve Function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()  # Check if inverse is cached
+  
+  if (!is.null(inv)) {
+    message("Getting cached data")  # Inform user that cached data is used
+    return(inv)
+  }
+  
+  data <- x$get()  # Retrieve the matrix
+  inv <- solve(data, ...)  # Compute the inverse
+  x$setinverse(inv)  # Store the inverse in cache
+  
+  inv
 }
+
+example:
+
+mat <- matrix(c(2, 1, 1, 4), 2, 2)
+cachedMat <- makeCacheMatrix(mat)
+
+cacheSolve(cachedMat)
+           [,1]       [,2]
+[1,]  0.5714286 -0.1428571
+[2,] -0.1428571  0.2857143
+
+cacheSolve(cachedMat)
+Getting cached data
+           [,1]       [,2]
+[1,]  0.5714286 -0.1428571
+[2,] -0.1428571  0.2857143
